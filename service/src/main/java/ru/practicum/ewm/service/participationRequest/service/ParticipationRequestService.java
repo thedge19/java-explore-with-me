e876e0171587d76f106ca6a19e2a1b1ee43dfa24
thedge <paz80@yandex.ru>
2025-05-20 23:12:32 +0300

    fix/ #28

--- service/src/main/java/ru/practicum/ewm/service/participationRequest/service/ParticipationRequestService.java
+++ service/src/main/java/ru/practicum/ewm/service/participationRequest/service/ParticipationRequestService.java
@@ -8,9 +8,7 @@ public interface ParticipationRequestService {
 
     List<ParticipationRequestDto> getAll(long userId);
 
-
     ParticipationRequestDto create(long userId, long eventId);
-
 
     ParticipationRequestDto patch(long userId, long requestId);
 }
