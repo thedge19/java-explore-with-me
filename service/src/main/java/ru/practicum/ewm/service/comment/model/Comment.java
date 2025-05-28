4c74f825272a664dcd3824bdb22b3f023b20b579
thedge <paz80@yandex.ru>
2025-05-28 13:36:22 +0300

    feat/ #1

--- service/src/main/java/ru/practicum/ewm/service/comment/model/Comment.java
+++ service/src/main/java/ru/practicum/ewm/service/comment/model/Comment.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "comments")
 public class Comment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
